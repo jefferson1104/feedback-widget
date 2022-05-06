e8f71bd2ef840c1cbed045be783d96b0677f19eb
jefferson1104 <jefferson1104junior@gmail.com>
2022-05-06 11:27:40 -0300

    BACK-END: create send email feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { primsa } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "147dbee3797ba6",
+    pass: "c6ca6689c0d300"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  });
+
+  await transport.sendMail({
+    from: 'Team Feedback <hello@feedget.com>',
+    to: 'Jefferson Soares <jefferson1104junior@gmail.com>',
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;" >`,
+      `<h1>Tipo do feedback: ${type}</h1>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
   });
   
   return res.status(201).json({ data: feedback });
