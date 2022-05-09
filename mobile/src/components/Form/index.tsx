a1855b3b623bdf9dc592421db3bda58a567e2be1
jefferson1104 <jefferson1104junior@gmail.com>
2022-05-09 09:56:52 -0300

    MOBILE: create Form component

---
+++ mobile/src/components/Form/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { 
+  View,
+  TextInput,
+  Image,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+import { ArrowLeft } from 'phosphor-react-native';
+import { FeedbackType } from '../../components/Widget';
+import { styles } from './styles';
+import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+interface FormProps {
+  feedbackType: FeedbackType;
+}
+
+export function Form({ feedbackType }: FormProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft 
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.titleContainer}>
+          <Image 
+            source={feedbackTypeInfo.image}
+            style={styles.image}
+          />
+          <Text style={styles.titleText}>
+            {feedbackTypeInfo.title}
+          </Text>
+        </View>
+      </View>
+
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="Type your feedback here and send..."
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+    </View>
+  );
+}
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -5,10 +5,13 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { Options } from '../Options';
+import { Form } from '../Form';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -36,7 +39,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form 
+          feedbackType="BUG"
+        />
       </BottomSheet>
     </>
   );
